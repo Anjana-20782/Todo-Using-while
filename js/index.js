bfeaceb19df2241b30a91c4bd6e27f53c5142094
Anjana-20782 <anjanasivadasan0305@gmail.com>
2025-10-20 20:19:10 +0530

    new contents added

---
+++ js/index.js
@@ -0,0 +1,11 @@
+let Todo=[];
+
+while(true){
+
+    let input=prompt("Enter input values \n [1] Add Task")
+
+    if(input=="1"){
+        let task=prompt("Enter Task Here");
+        Todo.push(task)
+    }
+}
